ea2491ce879738cafaa151a1c7cd658a86ad4d2e
H Rodrigues <38510364+hryds@users.noreply.github.com>
2025-01-13 12:40:31 -0300

    App bar e Side bar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,37 @@
 <template>
   <v-app>
+
+    <v-navigation-drawer permanent v-model="isDrawerOpen" color="#212c3a">
+      <v-list>
+        <v-list-subheader></v-list-subheader>
+        <v-list-item prepend-icon="mdi-fish">Reportar Produção</v-list-item>
+        <v-list-item prepend-icon="mdi-download">Extração de Dados</v-list-item>
+        <v-list-item prepend-icon="mdi-account-group">Gerenciar Usuários</v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar flat color="#3c8bbc" class="border-b">
+      <v-app-bar-nav-icon @click="isDrawerOpen = !isDrawerOpen"></v-app-bar-nav-icon>
+      <v-img
+      :src="logo"
+      max-height="150"
+      max-width="150"
+      contain
+      >
+      </v-img>
+    </v-app-bar>
     <v-main>
       <h1>Hello</h1>
     </v-main>
+  
   </v-app>
 </template>
 
-<script>
+<script setup>
+import logo from '@/assets/logo.png'
+import { ref } from 'vue';
 
+const isDrawerOpen = ref(false)
 </script>
 
 <style>
