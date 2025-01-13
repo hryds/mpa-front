b297e64ffa6865ce2f1b4f5f6ff78fd7aceb78bb
H Rodrigues <38510364+hryds@users.noreply.github.com>
2025-01-13 08:46:29 -0300

    Commit Inicial

---
+++ README.md
@@ -0,0 +1,14 @@
+# mpa-front
+
+## Project Setup
+### Dependencies
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+
+  </header>
+  <h1>Hello</h1>
+</template>
+
+<style scoped>
+</style>
